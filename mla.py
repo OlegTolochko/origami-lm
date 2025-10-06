6b1dc54a441d240d6c90c0cb9b3e52bded89f40e
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-10-06 17:45:41 +0200

    Preliminarily finish Rotational Positional Embedding calls for queries and keys, as well as the final attention score calucaltion

--- mla.py
+++ mla.py
@@ -41,24 +41,31 @@ class MultiHeadLatentAttention(nn.Module):
         self.register_buffer("kv_rope_cache")
 
     def forward(self, x: torch.Tensor, start_pos: int):
-        kv_c_new = self.dkv(x[start_pos:])  # (B, seq_len, d_compressed)
-        # TODO: RoPE for kv
+        kv_c = self.dkv(x[start_pos:])  # (B, seq_len, d_compressed)
+        kv_c_main, kv_c_rope = torch.split(
+            q_c, [self.d_model - self.d_compressed, self.d_compressed], -1
+        )
+
+        _, seq_len, _ = x.shape
+        rope_position_list = list(range(start_pos, seq_len))
         q_c = self.dq(x[start_pos:])
         q_c_main, q_c_rope = torch.split(
             q_c, [self.d_model - self.d_compressed, self.d_compressed], -1
         )
-        q_c_rope = apply_rotary_emb(q_c_rope)  # TODO
+        q_c_rope = apply_rotary_emb(q_c_rope, rope_position_list)
 
         kv_c_cached = self.kv_cache[:start_pos]
-        kv_c = torch.concat(kv_c_new, kv_c_cached)
+        kv_c = torch.concat(kv_c_main, kv_c_cached)
 
-        # TODO: Finish main Attention calculation part
-        q_k_c = q_c @ self.uk
+        kv_c_rope_new = apply_rotary_emb(kv_c_rope, rope_position_list)
+        kv_c_rope_cached = self.kv_rope_cache[:start_pos]
+        kv_c_rope = torch.concat(kv_c_rope_new, kv_c_rope_cached)
+
+        q_k_c = q_c_main @ self.uk
         q_k_c_head = torch.reshape(q_k_c, (-1, -1, self.n_head, self.head_dim))
         kv_c_head = torch.reshape(kv_c, (-1, -1, self.n_head, self.head_dim))
-        att_scores = q_k_c @ torch.transpose(kv_c)
+        att_scores = q_k_c_head @ torch.transpose(kv_c_head)
 
-        kv_c_head = torch.reshape((self.n_head, self.head_dim))
 
 
 args = ModelArgs()
